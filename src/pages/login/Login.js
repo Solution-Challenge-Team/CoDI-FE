8573e4daba2717d949190077113746fbc84676c4
wonhyuna <hyunawon417@gmail.com>
2024-02-05 17:18:36 +0900

    Feat: 로그인 페이지 [또는] 배치 완료 (#7)

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -13,6 +13,16 @@ const Login = () => {
         <input className="loginPassword" type = "password" placeholder="비밀번호"/>
         <button className="loginBtn">로그인</button>
       </div>
+
+      <div className="loginSep">
+        <div className="customHr"/>
+        <span className="orText">또는</span>
+        <div className="customHr" />
+      </div>
+
+      <div className="loginGoogle">
+
+      </div>
     </div>
   );
 };
