8f477b7fa666bc91de35a453f7c0b072a1958e96
wonhyuna <hyunawon417@gmail.com>
2024-02-08 22:47:28 +0900

    Feat: 병원 추천 페이지 병원 이름 (#28)

--- src/pages/hospital/HospitalItem.js
+++ src/pages/hospital/HospitalItem.js
@@ -2,12 +2,8 @@ import './hospital.css';
 const HospitalItem = ({id, hospital, address, symptom, evaluation, img}) =>{
   return(
     <div className="hospitalItem">
-      <p>id: {id}</p>
-      <p>hospital: {hospital}</p>
-      <p>address: {address}</p>
-      <p>symptom: {symptom}</p>
-      <p>evaluation: {evaluation}</p>
       <img src={img} alt="hospitalmain" className='hospitalMainImg'/>
+      <p className='hospitalName'>{hospital}</p>
     </div>
   )
 }
