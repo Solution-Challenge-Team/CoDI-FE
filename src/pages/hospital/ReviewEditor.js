788b387cb2f21bcd76c557e6615cf5c33170f70b
wonhyuna <hyunawon417@gmail.com>
2024-02-12 16:16:08 +0900

    Feat: 추천 병원 리뷰 input 창 (#50)

--- src/pages/hospital/ReviewEditor.js
+++ src/pages/hospital/ReviewEditor.js
@@ -1,5 +1,16 @@
+import './review.css';
 const ReviewEditor = () =>{
+  return (
+    <div className="reviewEditor">
+      <div className='editorTitle'>
+        <span className='editorTitleSpan'>[병원 리뷰 쓰기]</span>
+      </div>
+      <div className="reviewInput">
+        <textarea className="reviewTextarea"/>
 
+      </div>
+    </div>
+  )
 }
 
 export default ReviewEditor;
